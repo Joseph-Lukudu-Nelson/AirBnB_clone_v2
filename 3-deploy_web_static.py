6cf1f932b12d79965ce8ff2d268a4f9934429b5f
Joseph-Lukudu-Nelson <jkudus2022@gmail.com>
2023-04-10 16:12:43 +0300

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.26.167.180', '52.87.25.229']
 
 
 def do_pack():
